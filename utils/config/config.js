a277709d372c182be17e07f17e0c7f24e1695bee
esteban guzman <esteban.d.guzman@gmail.com>
2021-02-01 21:02:34 -0600

    config de migration

---
+++ utils/config/config.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const config = require('../../config')
+
+module.exports = {
+  development: {
+    username: config.db.user,
+    password: config.db.password,
+    database: config.db.database,
+    host: config.db.host,
+    port: config.db.port,
+    dialect: config.db.type,   
+  },
+  // test: {
+  //   username: process.env.CI_DB_USERNAME,
+  //   password: process.env.CI_DB_PASSWORD,
+  //   database: process.env.CI_DB_NAME,
+  //   host: '127.0.0.1',
+  //   port: 3306,
+  //   dialect: 'mysql',
+  //   dialectOptions: {
+  //     bigNumberStrings: true
+  //   }
+  // },
+  // production: {
+  //   username: process.env.PROD_DB_USERNAME,
+  //   password: process.env.PROD_DB_PASSWORD,
+  //   database: process.env.PROD_DB_NAME,
+  //   host: process.env.PROD_DB_HOSTNAME,
+  //   port: process.env.PROD_DB_PORT,
+  //   dialect: 'mysql',
+  //   dialectOptions: {
+  //     bigNumberStrings: true,
+  //     ssl: {
+  //       ca: fs.readFileSync(__dirname + '/mysql-ca-master.crt')
+  //     }
+  //   }
+  // }
+};
