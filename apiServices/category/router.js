697a12134988ed0564e256a40b3ca7360124c43c
esteban guzman <esteban.d.guzman@gmail.com>
2021-02-04 19:00:30 -0600

    is create function for consult category

--- apiServices/category/router.js
+++ apiServices/category/router.js
@@ -1,7 +1,9 @@
-const { createCategory }=  require('./controller')
+const { createCategory, consultCategory}=  require('./controller')
 
 const categoryRouter = (router) => {
     router.post('/categoria',createCategory)    
+    router.get('/categoria',consultCategory)
 }
+
 
 module.exports = categoryRouter
